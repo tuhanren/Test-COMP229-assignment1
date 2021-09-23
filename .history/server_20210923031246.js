08adc083b963d0116a965ac8ce6699a603a51d7b
tuhanren <tu.hanren@gmail.com>
2021-09-23 03:17:31 -0400

    before typescript

--- .history/server_20210923031246.js
+++ .history/server_20210923031246.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-let app = require("./Server/Config/app");
+let app = require("./Config/app");
 let debug = require("debug")("assignment1tt:server");
 let http = require("http");
 
