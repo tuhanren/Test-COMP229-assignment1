5f5c3272ceef68fed06545e389377c46c9d52d8b
Hanren Tu <tu.hanren@gmail.com>
2021-09-26 18:36:49 -0400

    about page

--- Server/Routes/index.js
+++ Server/Routes/index.js
@@ -1,37 +1,40 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const fs_1 = __importDefault(require("fs"));
 const router = express_1.default.Router();
 exports.default = router;
-
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Home" });
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Home' });
 });
 /* GET home page. */
-router.get("/home", function (req, res, next) {
-  res.render("index", { title: "Home" });
+router.get('/home', function (req, res, next) {
+    res.render('index', { title: 'Home' });
 });
 /* GET about us page. */
-router.get("/about", function (req, res, next) {
-  res.render("index", { title: "About" });
+router.get('/about', function (req, res, next) {
+    let resumePath = 'Public/Assets/docs/Hanren-Resume 2022 v2-SE.pdf';
+    fs_1.default.readFile(resumePath, function (err, data) {
+        res.contentType('application/pdf');
+        res.send(data);
+    });
 });
 /* GET Projects page. */
-router.get("/projects", function (req, res, next) {
-  res.render("index", { title: "Projects" });
+router.get('/projects', function (req, res, next) {
+    res.render('index', { title: 'Projects' });
 });
 /* GET services page. */
-router.get("/services", function (req, res, next) {
-  res.render("index", { title: "Services" });
+router.get('/services', function (req, res, next) {
+    res.render('index', { title: 'Services' });
 });
 /* GET contact us page. */
-router.get("/contact", function (req, res, next) {
-  res.render("index", { title: "Contact" });
+router.get('/contact', function (req, res, next) {
+    res.render('index', { title: 'Contact' });
 });
 //module.exports = router;
 //# sourceMappingURL=index.js.map
--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,9 +1,9 @@
 "use strict";
 // Immediately Invoked Function Expression
 (function () {
-  function Start() {
-    console.log("Start...");
-  }
-  window.addEventListener("load", Start);
+    function Start() {
+        console.log("Start...");
+    }
+    window.addEventListener("load", Start);
 })();
 //# sourceMappingURL=app.js.map
